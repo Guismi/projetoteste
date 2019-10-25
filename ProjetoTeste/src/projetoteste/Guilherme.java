178fcd989ae50cda9f31d7e4946a4b03888883ea
819167019 <819167019@STNLI0123.animaedu.intranet>
2019-10-25 20:44:13 -0300

    Versão 2.0

--- ProjetoTeste/src/projetoteste/Guilherme.java
+++ ProjetoTeste/src/projetoteste/Guilherme.java
@@ -5,6 +5,7 @@ public class Guilherme {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Ich bin Guilherme");
+System.out.println("Ich bin Guilherme de novo!");
 	}
 
 }
